e99a7244801e14b49b65b2f08d3ed34132ffcabd
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2019-12-17 22:09:16 -0600

    Commit inicial del proyecto

---
+++ Exchange_Rates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exchange_Rates
+//
+//  Created by David Ali on 17/12/19.
+//  Copyright © 2019 David Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
